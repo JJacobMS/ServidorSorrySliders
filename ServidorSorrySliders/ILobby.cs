a44ff8b3bef7c676a17431dac208cb39bc794871
melus7n7 <sulem477@gmail.com>
2023-10-14 00:52:53 -0600

    Lobby Servicio

---
+++ ServidorSorrySliders/ILobby.cs
@@ -0,0 +1,31 @@
+﻿using DatosSorrySliders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServidorSorrySliders
+{
+    [ServiceContract]
+    public interface ILobby
+    {
+        [OperationContract(IsOneWay = true)]
+        void EntrarPartida(string uid, string correoJugadorNuevo);
+        [OperationContract]
+        (Constantes, List<CuentaSet>) UnirseAlLobby(string uid, string correoJugadorNuevo);
+    }
+
+    [ServiceContract]
+    public interface ILobbyCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void JugadorEntroPartida(List<CuentaSet> listaJugadores);
+    }
+}
+
+
+// Crear un uid cuando crea 
+// EntrarPartida Cliente -> Servidor (One way)
+// JugadorEntroPartida Servidor -> Cliente (One way) Callback
