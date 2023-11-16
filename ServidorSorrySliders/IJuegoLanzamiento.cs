855a2957e565bf7fb5a60d7d225dda15a3ac6969
melus7n7 <sulem477@gmail.com>
2023-11-15 21:57:53 -0600

    IJuego lanzamiento

---
+++ ServidorSorrySliders/IJuegoLanzamiento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesServidorSorrySliders
+{
+    [ServiceContract]
+    public interface IJuegoLanzamiento
+    {
+        [OperationContract(IsOneWay = true)]
+        void AgregarJugadorJuegoLanzamiento();
+        [OperationContract(IsOneWay = true)]
+        void EliminarJugadorJuegoLanzamiento();
+    }
+}
